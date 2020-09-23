5dd8c95f028f87feb3e7252aa9deaabc8c6a6bb0
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-09-23 19:32:09 +1000

    allow debugger

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   rules: {
     // allow debugger during development
     "no-console": "off",
+    "no-debugger": "off",
     // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "vue/component-name-in-template-casing": ["error", "PascalCase"]
