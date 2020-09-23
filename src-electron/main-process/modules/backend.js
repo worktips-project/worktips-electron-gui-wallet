a4ebf3748c7ad64c5d4f334786913cfe08811792
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-09-23 16:27:51 +1000

    remove normal file log stream

--- src-electron/main-process/modules/backend.js
+++ src-electron/main-process/modules/backend.js
@@ -365,11 +365,6 @@ export class Backend {
           level: "trace",
           period: "1d", // daily rotation
           count: 4 // keep 4 days of logs
-        },
-        {
-          type: "file",
-          path: path.join(logPath, "electron.log"),
-          level: "trace"
         }
       ]
     });
