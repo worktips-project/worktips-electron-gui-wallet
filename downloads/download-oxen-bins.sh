e987bf652d1a97dcbb53972f9f2990433339394c
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-01-07 16:49:22 +1100

    use oxen bins for dev-bins and dev branch

---
+++ downloads/download-oxen-bins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# THIS IS TEMPORARY. TO BE REMOVED ONCE OXEN STUFF IS IN STABLE ON LOKI/OXEN CORE
+
+set -e
+
+if [ -z "$OS" ]; then
+    echo "OS must be set"
+    exit 1
+fi
+
+if [ -z "$RENAME" ]; then
+    RENAME="latest"
+fi
+
+if [ "$OS" == "Linux" ]; then
+    ASSET_URL="https://oxen.rocks/loki-project/loki-core/oxen-linux-x86_64-9.0.0-dev-d198af3de.tar.xz"
+elif [ "$OS" == "Windows" ]; then
+    ASSET_URL="https://oxen.rocks/loki-project/loki-core/oxen-win-x64-9.0.0-dev-d198af3de.zip"
+elif [ "$OS" == "macOS" ]; then
+    ASSET_URL="https://oxen.rocks/loki-project/loki-core/oxen-macos-x64-9.0.0-dev-d198af3de.zip"
+else
+    echo "OS must be Linux, Windows or macOS"
+    exit 1
+fi
+
+echo "About to download the binaries"
+
+curl -sL --fail \
+    -H "Accept: application/octet-stream" \
+    -o "${RENAME}" \
+    "$ASSET_URL"
+
+echo "Oxen binaries downloaded"
