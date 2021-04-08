72ff2d6b53a3bd6f4bf861fe7f5783c11ca1e3dc
Jason Rhinelander <jason@imaginary.ca>
2021-04-08 20:37:16 -0300

    Add braces like it's a 12-year old

--- src/mixins/service_node_mixin.js
+++ src/mixins/service_node_mixin.js
@@ -2,14 +2,17 @@ export default {
   methods: {
     // Returns the atomic, unfilled, reserved contributions for the given wallet
     getUnfilledReservedContribution(node, addr) {
-      for (const contributor of node.contributors)
-        if (contributor.address === addr && contributor.amount === 0 && contributor.reserved > 0)
+      for (const contributor of node.contributors) {
+        if (contributor.address === addr && contributor.amount === 0 && contributor.reserved > 0) {
           return contributor.reserved;
+        }
+      }
       return 0;
     },
     getMinContribution(node, myaddr) {
-      if (node.funded)
+      if (node.funded) {
         return 0;
+      }
 
       const MAX_NUMBER_OF_CONTRIBUTORS = 4;
       // If we have a reserved spot then that is our minimum:
@@ -32,8 +35,9 @@ export default {
         node.staking_requirement > node.total_reserved
           ? node.staking_requirement - node.total_reserved
           : 0;
-      if (addr)
+      if (addr) {
         openContributionRemaining += this.getUnfilledReservedContribution(node, addr);
+      }
       return openContributionRemaining;
     },
     openForContributionOxen(node, addr = null) {
